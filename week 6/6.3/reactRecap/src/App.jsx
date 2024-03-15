94d070e61cf263082daac7d4f0aab9971aad0465
Parag2812 <imparag2812@gmail.com>
2024-03-16 00:44:04 +0530

    6.3 added

---
+++ week 6/6.3/reactRecap/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button onClick={function (){
+        setCount (count +1);
+
+      }}>count is {count}</button>
+
+
+    </div>
+  )
+}
+
+export default App
