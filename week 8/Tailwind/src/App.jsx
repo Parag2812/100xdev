7bf3e2a64cc107b64d0529758234aba662a3015e
Parag2812 <imparag2812@gmail.com>
2024-03-21 01:55:58 +0530

    week 8

---
+++ week 8/Tailwind/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='bg-red-500 '>HI there 1</div>
+        <div className='bg-yellow-500 '>HI there 2</div>
+        <div className='bg-green-500 '>HI there 3</div>
+       </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+{/* <>
+<div className='grid grid-cols-10'>
+  <div className='bg-red-500 col-span-4'>HI</div>
+  <div className='bg-yellow-500 col-span-4'>HI</div>
+  <div className='bg-green-500 col-span-2'>HI</div>
+ </div>
+</> */}
